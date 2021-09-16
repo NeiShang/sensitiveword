88c334486b5e25e6925cba84794722cbdbb950d4
WuhuFlying <360786367@qq.com>
2021-09-16 17:03:26 +0800

    最终版本

--- 031902313/main.py
+++ 031902313/main.py
@@ -4,29 +4,26 @@ from transform import *
 
 class Filter:
     def __init__(self):
-        self.sensitive_words = {}
-        self.delimit = 'end'
-        self.sensitive_words_dict = {}
+        self.sensitive_words = {}  # 敏感词树
+        self.delimit = 'end'  # 结束符
+        self.sensitive_words_dict = {}  # 敏感词字典，配合结束符判断敏感词类型
 
-    def parse_sensitive_words(self, path):
-        file = open(path, 'r', encoding='utf-8')
-        text = file.read()
-        sensitive_words_list = text.split('\n')
+    def parse_sensitive_words(self, sensitive_words_list):  # 构建敏感词字典，并获得敏感词组合用于构建敏感词树
         count = 0
-        for sensitive_words in sensitive_words_list:
-            self.sensitive_words_dict[count] = ' <'+sensitive_words+'> '
-            combinations = word_combination(sensitive_words)
+        for sensitive_word in sensitive_words_list:
+            self.sensitive_words_dict[count] = ' <' + sensitive_word + '> '  # 增加敏感词字典
+            combinations = word_combination(sensitive_word)  # 获得敏感词组合
             for word in combinations:
-                self.add_sensitive_words(word, count)
+                self.add_sensitive_words(word, count)  # 构建敏感词树
             count += 1
 
-    def add_sensitive_words(self, sensitive_word, count):
+    def add_sensitive_words(self, sensitive_word, count):  # 构建敏感词树
         chars = sensitive_word.lower()
         if not chars:
             return
         level = self.sensitive_words
         for i in range(len(chars)):
-            if is_chinese(chars[i]):
+            if is_chinese(chars[i]):  # 判断是否是汉字，如果是汉字转换为拼音
                 word_pinyin = ''.join(lazy_pinyin(chars[i]))
             else:
                 word_pinyin = chars[i]
@@ -49,16 +46,15 @@ class Filter:
             if i == len(chars) - 1:
                 level[self.delimit] = count
 
-    def sensitive_words_filter(self, input_path, output_path):
-        input_file = open(input_path, 'r', encoding='utf-8')
-        output_file = open(output_path, 'w+', encoding='utf-8')
+    def sensitive_words_filter(self, org_file):  # 敏感词检测
         current_row = 0
         total_count = 0
-        for line in input_file:
+        content = ''
+        for line in org_file:
             line_copy = line
             line = line.lower()
             current_row += 1
-            start_flag = False
+            start_flag = False  # start_flag 用来标记是否已经进入敏感词树
             start = 0
             while start < len(line):
                 level = self.sensitive_words
@@ -69,40 +65,43 @@ class Filter:
                     if start_flag and is_other(char):
                         step += 1
                         continue
-                    char = ''.join(lazy_pinyin(char))
+                    char = ''.join(lazy_pinyin(char))  # 将汉字转换成拼音
                     if char in level:
                         start_flag = True
                         step += 1
                         if self.delimit not in level[char]:
                             level = level[char]
                         else:
-                            keywords = line_copy[start:start+step]
+                            keywords = line_copy[start:start + step]
                             start += step - 1
                             total_count += 1
-                            output_file.write('Line'+str(current_row)+':'
-                                              + self.sensitive_words_dict[level[char][self.delimit]] + keywords+'\n')
+                            content = "{}{}{}{}{}{}{}".format(content, 'Line', str(current_row), ':',
+                                                              self.sensitive_words_dict[
+                                                                  level[char][self.delimit]], keywords, '\n')
                             start_flag = False
                             break
                     else:
                         start_flag = False
                         break
                 start += 1
-        input_file.close()
-        output_file.close()
-        with open(output_path, "r+", encoding='utf-8') as f:
-            old = f.read()
-            f.seek(0)
-            f.write('total: ' + str(total_count) + '\n')
-            f.write(old)
+        content = "{}{}".format('total: ' + str(total_count) + '\n', content)
+        return content
 
 
 if __name__ == '__main__':
     try:
         org, words, test = sys.argv[1:4]
     except Exception as e:
-        print(sys.argv)
         print(e)
+        exit(0)
     sensitive_filter = Filter()
-    sensitive_filter.parse_sensitive_words(sys.argv[1])
-    sensitive_filter.sensitive_words_filter(sys.argv[2], sys.argv[3])
-
+    with open(sys.argv[1], 'r', encoding='utf-8') as f:
+        text = f.read()
+        sensitive_words = text.split('\n')
+        sensitive_filter.parse_sensitive_words(sensitive_words)
+    with open(sys.argv[2], 'r', encoding='utf-8') as input_file:
+        text = input_file.read()
+        input_content = text.split('\n')
+        ans = sensitive_filter.sensitive_words_filter(input_content)
+    with open(sys.argv[3], 'w+', encoding='utf-8') as output_file:
+        output_file.write(ans)
