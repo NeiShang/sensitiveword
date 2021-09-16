0ccae0c7e76a03f410b291b06abcc931d2e7fc45
WuhuFlying <360786367@qq.com>
2021-09-16 22:15:12 +0800

    change unit_test.py

--- 031902313/test_unit.py
+++ 031902313/test_unit.py
@@ -11,10 +11,10 @@ class TestClass:
         assert ans == result
 
     def test_filter(self):
-        sensitive_words = ['操你妈', 'sb']
-        text = ['cao妳媽的傻逼S12（b', '扌喿亻尔女马挥洒及我们把手机是你', 's  b就， 扫平今晚我操n嘛年四蛇女cnm']
-        ans = 'total: 6\nLine1: <操你妈> cao妳媽\nLine1: <sb> S12（b\nLine2: <操你妈> 扌喿亻尔女马\nLine3: <sb> s  b\n' \
-              'Line3: <操你妈> 操n嘛\nLine3: <操你妈> cnm\n'
+        sensitive_words = ['你好', 'hello']
+        text = ['亻尔女子hE’；0llo', '好睡觉哦i你手机n女子，是孔金瓯省亻尔h暑假', '妳；】‘、。好你ha睡觉哦i啊']
+        ans = 'total: 6\nLine1: <你好> 亻尔女子\nLine1: <hello> hE’；0llo\nLine2: <你好> n女子\n' \
+              'Line2: <你好> 亻尔h\nLine3: <你好> 妳；】‘、。好\nLine3: <你好> 你h\n'
         test = Filter()
         test.parse_sensitive_words(sensitive_words)
         result = test.sensitive_words_filter(text)
